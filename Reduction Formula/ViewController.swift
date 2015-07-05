c1b5ad6dc00f9bfb9093a29f5d868628ac549f8d
Tomisall <Tom@Thomass-Air-2.lan>
2015-07-05 14:44:42 +0100

    Initial Commit

---
+++ Reduction Formula/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reduction Formula
+//
+//  Created by Thomas Sheridan on 05/07/2015.
+//  Copyright (c) 2015 Thomas Sheridan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Reduction FormulaTests/Reduction_FormulaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Reduction_FormulaTests.swift
+//  Reduction FormulaTests
+//
+//  Created by Thomas Sheridan on 05/07/2015.
+//  Copyright (c) 2015 Thomas Sheridan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Reduction_FormulaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
